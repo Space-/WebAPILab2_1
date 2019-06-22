edac85a5419c35ae2688b4797a4b63856ac54a19
Bruce <gkm85663@gmail.com>
2019-06-22 10:21:53 +0800

    Create web api project

---
+++ WebAPILab2_1/Controllers/TestController.cs
@@ -0,0 +1,40 @@
+﻿using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPILab2_1.Controllers
+{
+    public class TestController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            return new HttpResponseMessage()
+            {
+                Content = new StringContent("Get from test.")
+            };
+        }
+
+        public HttpResponseMessage Post()
+        {
+            return new HttpResponseMessage()
+            {
+                Content = new StringContent("Post from test.")
+            };
+        }
+
+        public HttpResponseMessage Put()
+        {
+            return new HttpResponseMessage()
+            {
+                Content = new StringContent("Put from test.")
+            };
+        }
+
+        public HttpResponseMessage Delete()
+        {
+            return new HttpResponseMessage()
+            {
+                Content = new StringContent("Delete from test.")
+            };
+        }
+    }
+}
